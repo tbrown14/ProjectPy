cedae2e539eba59ac119b5700968ce342692336e
Travis Brown <tbrown14@confederationcollege.ca>
2020-04-10 12:15:04 -0400

    Deleted unwanted comments in code

--- theMathProj.py
+++ theMathProj.py
@@ -1,12 +1,4 @@
-# num1 = 5
 
-# num2 = 3
-
-# def mathFunc(num1,num2):
-#   result = num1 + num2
-#   return result
-  
-# answer = mathFunc(num1,num2)
  
 def hello_Func(num1,num2):
   result = num1 + num2
@@ -17,10 +9,7 @@ ans = hello_Func(80,100)
 print(ans)
 
 #Comment Test
-#Blahtest
-#HiAdam
 
-#wow
 
 x = 3
 y =7
