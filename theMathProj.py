91fdd7fbf578981c72d33b7f9442b7e84cfd8f65
Travis Brown <tbrown14@confederationcollege.ca>
2020-04-10 11:56:02 -0400

    Added a python file with a math function

---
+++ theMathProj.py
@@ -0,0 +1,19 @@
+# num1 = 5
+
+# num2 = 3
+
+# def mathFunc(num1,num2):
+#   result = num1 + num2
+#   return result
+  
+# answer = mathFunc(num1,num2)
+ 
+def hello_Func(num1,num2):
+  result = num1 + num2
+  return result
+
+ans = hello_Func(80,100)
+
+print(ans)
+
+#Comment Test
