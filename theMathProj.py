d4ab480ec9d05a9ed5f03f71a3b8acf7ecf4e023
Travis Brown <tbrown14@confederationcollege.ca>
2020-04-15 10:05:52 -0400

    Added a funtion to times number up to a specified input value

--- theMathProj.py
+++ theMathProj.py
@@ -28,11 +28,25 @@ import math
     
     #something
 
-l=[]
-for i in range(2000, 3201):
-  if(i%7==0) and (i%5!=0):
-    l.append(str(i))
+# l=[]
+# for i in range(2000, 3201):
+#   if(i%7==0) and (i%5!=0):
+#     l.append(str(i))
 
-print(','.join(l))
+# print(','.join(l))
+
+# def fact(x):
+#     if x == 0:
+#         return 1
+#     return x * fact(x - 1)
+
+# x=int(input())
+# print (fact(x))
+
+# n=int(input())
+# d=dict()
+# for i in range(1,n+1):
+#   d[i]=i*i
+# print (d)
       
 
