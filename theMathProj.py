89185a83c9a5522710508c5edd08f1290f1713e2
Travis Brown <tbrown14@confederationcollege.ca>
2020-04-15 12:27:50 -0400

    Added calculator code

--- theMathProj.py
+++ theMathProj.py
@@ -98,6 +98,53 @@ import math
 
 # print(multilist)
 
-s = input()
-words = [word for word in s.split(" ")]
-print (" ".join(sorted(list(set(words)))))
+# s = input()
+# words = [word for word in s.split(" ")]
+# print (" ".join(sorted(list(set(words)))))
+
+# value = []
+# items=[x for x in input().split(',')]
+# for p in items:
+#     intp = int(p, 2)
+#     if not intp%5:
+#         value.append(p)
+
+# print (','.join(value))       
+
+#Check if a nuber in the range 1-101 is divisable by 5 and 3 
+#FIZZ BUZZ 
+# for num in range(1,101):
+#     if num % 5 == 0 and num % 3 == 0:
+#         print("FIZZBUZZ")
+#     elif num % 3 == 0:
+#         print("FIZZ")
+#     elif num % 5:
+#         print("BUZZ")
+#     else:
+#         print (num)
+
+# #Fibonacci Sequence 
+
+# a,b = 0,1
+# for i in range(0,10):
+#     print(a)
+#     a,b = b, a + b
+
+#Dictionary 
+
+# my_dict = {'name': 'Bronx', 'age': '22', 'WorkType': 'Programmer'}
+#     for key,val in my_dict.items():
+#         print("my {} is {}".format(key,val))
+
+tax = 0.13
+price = input()
+
+def calc(price, tax):
+    
+    calcTax = float(price) * tax
+    total = calcTax + float(price)
+    return total
+    
+
+answer = calc(price, tax)
+print(answer)
