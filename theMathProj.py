5da26721ef5223c4a417d1e62938b3bec8dc26e5
Travis Brown <tbrown14@confederationcollege.ca>
2020-04-15 09:56:33 -0400

    Added a for loop that finds all numbers that are divis by 7 and 5 between the range of 2000, 3201

--- theMathProj.py
+++ theMathProj.py
@@ -1,27 +1,38 @@
-num1 = input('Enter your first number ')
-num2 = input('Enter your second number ')
+import math
+
+# num1 = input('Enter your first number ')
+# num2 = input('Enter your second number ')
  
-def math_Func(num1,num2):
-  result = int(num1) + int(num2)
-  return result
+# def math_Func(num1,num2):
+#   result = int(num1) + int(num2)
+#   return result
 
-ans = math_Func(num1,num2)
+# ans = math_Func(num1,num2)
 
-print(ans)
+# print(ans)
 
-#Comment Test
+# #Comment Test
 
 
-x = input('Enter your first number ')
-y = input('Enter your second number ')
+# x = input('Enter your first number ')
+# y = input('Enter your second number ')
 
-if(x>y):
-    print('X is greater than y')
-elif (x<y):
-        print('Y is greater than x')
+# if(x>y):
+#     print('X is greater than y')
+# elif (x<y):
+#         print('Y is greater than x')
 
         #test
 
         #Change
     
     #something
+
+l=[]
+for i in range(2000, 3201):
+  if(i%7==0) and (i%5!=0):
+    l.append(str(i))
+
+print(','.join(l))
+      
+
